d47bffb43fdd5a979aa1c1d7ca2a2598ac5c65e3
Dev PA <devpa@itsoft.com>
2021-11-22 09:57:38 +0530

    test

--- Client/wwwroot/service-worker.published.js
+++ Client/wwwroot/service-worker.published.js
@@ -37,10 +37,10 @@ async function onFetch(event) {
     if (event.request.method === 'GET') {
         // For all navigation requests, try to serve index.html from cache
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
-        const shouldServeIndexHtml = event.request.mode === 'navigate'
-            && !event.request.url.includes('/');
+        const shouldServeIndexHtml = event.request.mode === 'navigate';
 
-        const request = shouldServeIndexHtml ? 'index.html' : event.request;
+       // const request = shouldServeIndexHtml ? 'index.html' : event.request;
+       const request =  event.request;
         const cache = await caches.open(cacheName);
         cachedResponse = await cache.match(request);
     }
