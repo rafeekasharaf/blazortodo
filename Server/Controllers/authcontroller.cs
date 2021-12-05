79893105729f61b4414bdbc0dfe9608b0bbd76d3
Dev PA <devpa@itsoft.com>
2021-12-05 08:57:34 +0530

    Try to fix GoogleAuth

--- Server/Controllers/authcontroller.cs
+++ Server/Controllers/authcontroller.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Server.Controllers
 {
@@ -15,7 +18,17 @@ namespace Server.Controllers
         [HttpGet("google-login")]
         public async Task GoogleAuth() {
             
-            await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = "/"});
+         /* string returnUrl = "/signin-google";
+            await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, 
+            new AuthenticationProperties { 
+               RedirectUri = "/"
+            //    RedirectUri = Url.Page("/", 
+            //     pageHandler: "Callback", 
+            //     values: new { returnUrl }),
+            });*/
+
+            var properties = new AuthenticationProperties { RedirectUri = Url.Action("GoogleResponse") };
+        await HttpContext.ChallengeAsync( GoogleDefaults.AuthenticationScheme, properties);
 
             // await HttpContext.ChallengeAsync(
             //     prop,
@@ -24,6 +37,45 @@ namespace Server.Controllers
 
             } 
         }
+
+        [Route("google-response")]
+    public async Task<IActionResult> GoogleResponse()
+    {
+        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+ 
+        var claims = result.Principal.Identities
+            .FirstOrDefault().Claims.Select(claim => new
+        {
+            claim.Issuer,
+            claim.OriginalIssuer,
+            claim.Type,
+            claim.Value
+        });
+ 
+       // return JsonResult(claims);
+       return LocalRedirect("/");
+    }
+
+        public async Task<IActionResult> OnGetCallbackAsync(
+            string returnUrl = null, string remoteError = null)
+        {
+            // Get the information about the user from the external login provider
+            var GoogleUser = this.User.Identities.FirstOrDefault();
+            if (GoogleUser.IsAuthenticated)
+            {
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = true,
+                    RedirectUri = this.Request.Host.Value
+                };
+                await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(GoogleUser),
+                authProperties);
+            }
+            return LocalRedirect("/ffff");
+        }
+    
 
         [HttpGet("logoutuser")]
         public async Task<ActionResult<String>> LogOutUser()
